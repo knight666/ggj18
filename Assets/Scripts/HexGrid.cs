f4fd4eaf8eee75628cbacfa2f9ed7f6fd2a8a080
Quinten Lansu <quintenlansu@gmail.com>
2018-01-27 08:57:09 +0100

    Attach hex grid renderer to camera.

---
+++ Assets/Scripts/HexGrid.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexGrid : MonoBehaviour
+{
+	public Material m_material;
+	private Vector3 m_startVertex = new Vector3(0, 0, 0);
+	private Vector3 m_cursorPosition;
+
+	void Update()
+	{
+		m_cursorPosition = Input.mousePosition;
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			m_startVertex = new Vector3(m_cursorPosition.x / Screen.width, m_cursorPosition.y / Screen.height, 0);
+		}
+	}
+
+	void OnPostRender()
+	{
+		if (!m_material)
+		{
+			Debug.LogError("Please Assign a material on the inspector");
+			return;
+		}
+
+		GL.PushMatrix();
+		m_material.SetPass(0);
+		GL.LoadOrtho();
+		GL.Begin(GL.LINES);
+		GL.Color(Color.red);
+		GL.Vertex(m_startVertex);
+		GL.Vertex(new Vector3(m_cursorPosition.x / Screen.width, m_cursorPosition.y / Screen.height, 0));
+		GL.End();
+		GL.PopMatrix();
+	}
+}
