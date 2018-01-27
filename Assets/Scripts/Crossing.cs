5ee5600ac5ed23a43006da72bbffd19b6524ae63
Quinten Lansu <quintenlansu@gmail.com>
2018-01-27 10:15:58 +0100

    Update hex grid.

---
+++ Assets/Scripts/Crossing.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crossing : MonoBehaviour {
+
+	void Start ()
+	{
+	}
+
+	void Update ()
+	{
+		var pos = this.GetComponent<Transform>().position;
+		Debug.DrawLine(pos - new Vector3(10.0f, 10.0f), pos + new Vector3(10.0f, 10.0f));
+	}
+}
--- Assets/Scripts/HexGrid.cs
+++ Assets/Scripts/HexGrid.cs
@@ -7,6 +7,36 @@ public class HexGrid : MonoBehaviour
 	public Material m_material;
 	private Vector3 m_startVertex = new Vector3(0, 0, 0);
 	private Vector3 m_cursorPosition;
+	private Matrix4x4 m_cubeRotate = new Matrix4x4(
+		new Vector4(Mathf.Sqrt(3.0f),         0,            0.0f, 0.0f),
+		new Vector4(Mathf.Sqrt(3.0f) / 2.0f,  3.0f / 2.0f,  0.0f, 0.0f),
+		new Vector4(0.0f,                     0.0f,         0.0f, 0.0f),
+		new Vector4(0.0f,                     0.0f,         0.0f, 1.0f)
+	);
+	private Vector3[] m_corners = new Vector3[6];
+
+	public const float outerRadius = 10f;
+	public const float innerRadius = outerRadius * 0.866025404f;
+	public static Vector3[] corners = {
+		new Vector3(0f, 0f, outerRadius),
+		new Vector3(innerRadius, 0f, 0.5f * outerRadius),
+		new Vector3(innerRadius, 0f, -0.5f * outerRadius),
+		new Vector3(0f, 0f, -outerRadius),
+		new Vector3(-innerRadius, 0f, -0.5f * outerRadius),
+		new Vector3(-innerRadius, 0f, 0.5f * outerRadius),
+		new Vector3(0f, 0f, outerRadius)
+	};
+
+	HexGrid()
+	{
+		m_cubeRotate *= Matrix4x4.Scale(new Vector3(10.0f, 10.0f, 1.0f));
+
+		for (var i = 0; i < 6; ++i)
+		{
+			var angle = Mathf.Deg2Rad * (30.0f + (i * 60.0f));
+			m_corners[i] = m_cubeRotate * new Vector3(Mathf.Cos(angle), Mathf.Sin(angle), 0.0f);
+		}
+	}
 
 	void Update()
 	{
@@ -25,13 +55,25 @@ public class HexGrid : MonoBehaviour
 			return;
 		}
 
+		var toScreenSpace = new Vector3(1.0f / Screen.width, 1.0f / Screen.height, 1.0f);
+
 		GL.PushMatrix();
 		m_material.SetPass(0);
 		GL.LoadOrtho();
 		GL.Begin(GL.LINES);
 		GL.Color(Color.red);
+
 		GL.Vertex(m_startVertex);
-		GL.Vertex(new Vector3(m_cursorPosition.x / Screen.width, m_cursorPosition.y / Screen.height, 0));
+		GL.Vertex(Vector3.Scale(m_cursorPosition, toScreenSpace));
+
+		var position = m_cursorPosition;
+		GL.Vertex(Vector3.Scale(corners[0] + position, toScreenSpace));
+
+		for (var i = 1; i < 6; ++i)
+		{
+			GL.Vertex(Vector3.Scale(corners[i] + position, toScreenSpace));
+		}
+
 		GL.End();
 		GL.PopMatrix();
 	}
