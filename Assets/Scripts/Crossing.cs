ff0cba902bba6122e12cb873bc70a540fee34f31
Quinten Lansu <quintenlansu@gmail.com>
2018-01-27 11:48:41 +0100

    Highlight active crossings near the bee.

--- Assets/Scripts/Crossing.cs
+++ Assets/Scripts/Crossing.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class Crossing : MonoBehaviour {
 
+	public bool m_active = false;
+
 	void Start ()
 	{
 	}
 
 	void Update ()
 	{
+		var sprite = GetComponent<SpriteRenderer>();
+		sprite.color = m_active ? Color.red : Color.white;
 	}
 }
