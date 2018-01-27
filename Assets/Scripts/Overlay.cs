6f59c9417612a17c57f8a41661865de417769cf4
Quinten Lansu <quintenlansu@gmail.com>
2018-01-27 15:57:11 +0100

    Add animation for overlay.

---
+++ Assets/Scripts/Overlay.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Overlay
+	: MonoBehaviour
+{
+	void Start()
+	{
+	}
+
+	void Update()
+	{
+	}
+
+	public void DoneEvent(string s)
+	{
+		Destroy(this);
+	}
+}
