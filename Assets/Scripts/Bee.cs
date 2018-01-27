c897cc3c251f645e728e11978c3122721f847099
Quinten Lansu <quintenlansu@gmail.com>
2018-01-27 12:34:22 +0100

    Highlight traversable cells on key press.

--- Assets/Scripts/Bee.cs
+++ Assets/Scripts/Bee.cs
@@ -25,13 +25,69 @@ public class Bee : MonoBehaviour {
 		var positionFlat = transform.position;
 		positionFlat.z = 0;
 
+		List<GameObject> nearCrossings = new List<GameObject>();
+
 		foreach (var crossing in m_hexGrid.m_controlPoints)
 		{
 			var crossingFlat = crossing.transform.position;
 			crossingFlat.z = 0;
 
 			var distance = Vector3.Distance(positionFlat, crossingFlat);
-			crossing.GetComponent<Crossing>().m_active = distance > m_distanceMinimum && distance < m_distanceMaximum;
+			var isNear = distance > m_distanceMinimum && distance < m_distanceMaximum;
+			if (isNear)
+			{
+				nearCrossings.Add(crossing);
+			}
+
+			//crossing.GetComponent<Crossing>().m_active = isNear;
+		}
+
+		if (Input.GetKeyDown(KeyCode.Q))
+		{
+			foreach (var crossing in m_hexGrid.m_controlPoints)
+			{
+				crossing.GetComponent<Crossing>().m_active = false;
+			}
+		}
+
+		if (nearCrossings.Count > 0 &&
+			Input.GetKeyDown(KeyCode.A))
+		{
+			positionFlat.y -= 1;
+
+			var greatestAngle = -Mathf.Infinity;
+			GameObject greatestAngleInstance = null;
+
+			var normalUp = new Vector3(0, 1, 0);
+			var normalRight = new Vector3(1, 0, 0);
+
+			foreach (var crossing in nearCrossings)
+			{
+				var crossingNormal = crossing.transform.position;
+				crossingNormal.z = 0;
+
+				crossingNormal = crossingNormal - positionFlat;
+				crossingNormal.Normalize();
+
+				/*float angle = Vector3.Angle(positionFlat, crossingFlat) - 180;
+				if (angle > greatestAngle)
+				{
+					greatestAngle = angle;
+					greatestAngleInstance = crossing;
+				}*/
+
+				float dotRight = Vector3.Dot(normalUp, crossingNormal);
+				float dotUp = Vector3.Dot(normalRight, crossingNormal);
+				if (dotRight > 0)
+				{
+					crossing.GetComponent<Crossing>().m_active = true;
+				}
+			}
+
+			if (greatestAngleInstance)
+			{
+				greatestAngleInstance.GetComponent<Crossing>().m_active = true;
+			}
 		}
 	}
 }
