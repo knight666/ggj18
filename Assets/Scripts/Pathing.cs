edb90ff0ebad4cbee8de27096897641416dc1395
Quinten Lansu <quintenlansu@gmail.com>
2018-01-28 10:16:39 +0100

    Add arrows to opening.

---
+++ Assets/Scripts/Pathing.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pathing
+{
+	public enum Directions
+	{
+		Left,
+		Right
+	};
+
+	public static Directions[][] m_paths = new Directions[3][] {
+		new Directions[] { Directions.Right, Directions.Right, Directions.Left, Directions.Left, Directions.Right, Directions.Left, Directions.Left, Directions.Right, Directions.Right },
+		new Directions[] { Directions.Right, Directions.Left, Directions.Right, Directions.Left, Directions.Left, Directions.Right, Directions.Right, Directions.Right, Directions.Right },
+		new Directions[] { Directions.Left, Directions.Right, Directions.Right, Directions.Right, Directions.Right, Directions.Left, Directions.Left, Directions.Left, Directions.Left }
+	};
+	public static List<Directions> m_currentPath = new List<Directions>(9);
+
+	public static void Initialize()
+	{
+		m_currentPath.AddRange(m_paths[0]);
+	}
+}
